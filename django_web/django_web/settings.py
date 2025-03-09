ff10609af74098a9ee2549438da6219085a0a97c
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-09 20:56:10 +0300

    //

--- django_web/django_web/settings.py
+++ django_web/django_web/settings.py
@@ -123,9 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = "static/"
-
-STATIC_ROOT = BASE_DIR / "staticfiles"  # This path should be used in production
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Without the trailing slash
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
