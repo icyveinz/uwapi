f056536a4ed521953910cf67d2d2988c1159284e
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-09 17:58:37 +0300

    //

---
+++ django_web/orders_client/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework import serializers
+
+class CustomerSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    name = serializers.CharField(max_length=255)
+    about_customer = serializers.CharField(max_length=1000)
--- django_web/orders_client/views.py
+++ django_web/orders_client/views.py
@@ -1,2 +1,23 @@
+from rest_framework.views import APIView
+from orders_client.models import Order
+from orders_client.serializers import CustomerSerializer
 
-# Create your views here.
+
+class CustomerCreateView(APIView):
+    def post(self, request):
+        serializer = CustomerSerializer(data=request.data)
+        if serializer.is_valid():
+            customer_data = serializer.validated_data
+
+            Order.objects.create(
+                parent="UGO",
+                status="Не обработан",
+                customer_name=customer_data['name'],
+                customer_email=customer_data['email'],
+                customer_description=customer_data['about_customer']
+            )
+
+            return Response({
+                "is_succeed": True,
+                "message" : "Ваш запрос был отправлен! Мы свяжемся с вами как можно скорее."
+            })
