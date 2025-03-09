b0d2eeae2d392c05937fa1200a0707ee3d1c3398
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-09 18:37:41 +0300

    //

--- django_web/orders_client/views.py
+++ django_web/orders_client/views.py
@@ -8,22 +8,28 @@ from orders_client.serializers import CustomerSerializer
 
 
 class CustomerCreateView(APIView):
-    def post(self, request):
+    def post(self, request, parent : str):
         serializer = CustomerSerializer(data=request.data)
         if serializer.is_valid():
             customer_data = serializer.validated_data
             try:
-                Order.objects.create(
-                    parent="UGO",
-                    status="Не обработан",
-                    customer_name=customer_data['name'],
-                    customer_email=customer_data['email'],
-                    customer_description=customer_data['about_customer']
-                )
-                return Response({
-                    "is_succeed": True,
-                    "message": rus['template_success']
-                }, status=status.HTTP_201_CREATED)
+                if parent == "ugo" or parent == "wgl":
+                    Order.objects.create(
+                        parent=parent.upper(),
+                        status="Не обработан",
+                        customer_name=customer_data['name'],
+                        customer_email=customer_data['email'],
+                        customer_description=customer_data['about_customer']
+                    )
+                    return Response({
+                        "is_succeed": True,
+                        "message": rus['template_success']
+                    }, status=status.HTTP_201_CREATED)
+                else:
+                    return Response({
+                        "is_succeed": False,
+                        "message": rus['src_error']
+                    }, status=status.HTTP_400_BAD_REQUEST)
 
             except Exception as e:
                 return Response({
@@ -33,5 +39,5 @@ class CustomerCreateView(APIView):
 
         return Response({
             "is_succeed": False,
-            "message": "Invalid data"
+            "message": rus['data_error']
         }, status=status.HTTP_400_BAD_REQUEST)
