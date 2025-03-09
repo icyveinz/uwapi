7453d715b98b853cd3a58730750ab3b647688ba6
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-09 22:06:57 +0300

    //

--- django_web/orders_client/admin.py
+++ django_web/orders_client/admin.py
@@ -1,12 +1,20 @@
 # Register your models here.
 from django.contrib import admin
-from orders_client.models import Order
+from orders_client.models import Order, Comment
 
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+    fields = ('comment', 'created_at')
+    readonly_fields = ['created_at']
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = ("customer_name", "status", "customer_email", "customer_description", "timestamp")
+    list_display = ( "parent", "status", "customer_name", "customer_email", "customer_description", "timestamp")
     search_fields = ["customer_name", "customer_email"]
     list_filter = ["status", "parent"]
     list_editable = ["status"]  # Делаем статус редактируемым
+    inlines = [CommentInline]
 
 admin.site.register(Order, OrderAdmin)
+admin.site.register(Comment)
