05c719f765c3e5afc72d13d10a41afff9e5aa26d
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-09 21:36:07 +0300

    //

--- django_web/orders_client/admin.py
+++ django_web/orders_client/admin.py
@@ -4,17 +4,9 @@ from orders_client.models import Order
 
 
 class OrderAdmin(admin.ModelAdmin):
-    list_display = (
-        "customer_name",
-        "customer_email",
-        "status",
-        "timestamp",
-    )  # Fields to display in list view
-    search_fields = [
-        "customer_name",
-        "customer_email",
-    ]  # Enable search by customer_name or customer_email
-    list_filter = ["status"]  # Add filters for status field
-
+    list_display = ("customer_name", "status", "customer_email", "customer_description", "timestamp")
+    search_fields = ["customer_name", "customer_email"]
+    list_filter = ["status", "parent"]
+    list_editable = ["status"]  # Делаем статус редактируемым
 
 admin.site.register(Order, OrderAdmin)
