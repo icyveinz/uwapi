18682e0f0b1d3309c1820e5a39245e5e8cf5dc2b
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-11 18:43:23 +0300

    //

--- nats_worker/telegram_actions.py
+++ nats_worker/telegram_actions.py
@@ -1,7 +1,14 @@
+import re
 from telegram import Bot
 from telegram.constants import ParseMode
 from config import Config
 from lexicon import rus
+
+
+def escape_markdown_v2(text: str) -> str:
+    # Define a list of characters that need to be escaped in MarkdownV2
+    dangerous_characters = r"([*_`\[\]()~>#+\-=|{}.!])"
+    return re.sub(dangerous_characters, r"\\\1", text)
 
 
 async def init_bot(config: Config):
@@ -13,7 +20,8 @@ async def init_bot(config: Config):
 
 
 def format_the_message(message: dict) -> str:
-    return rus["new_order"].format(**message)
+    formatted_message = rus["new_order"].format(**message)
+    return escape_markdown_v2(formatted_message)
 
 
 async def bot_send_message(config: Config, bot: Bot, message: str):
