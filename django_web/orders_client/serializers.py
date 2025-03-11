02487ad7f91889c04c21d066a48d735b7929a2b1
Egor Ivanov <rust_lang_lover@proton.me>
2025-03-11 18:08:04 +0300

    //

--- django_web/orders_client/serializers.py
+++ django_web/orders_client/serializers.py
@@ -1,7 +1,22 @@
 from rest_framework import serializers
+import re
+from lexicon import rus
 
 
 class CustomerSerializer(serializers.Serializer):
-    email = serializers.EmailField()
+    email = serializers.CharField(max_length=255)
     name = serializers.CharField(max_length=255)
     about_customer = serializers.CharField(max_length=1000)
+
+    def validate_email(self, value):
+        # Regular expression for validating a phone number (10-15 digits)
+        phone_regex = r"^\d{10,15}$"
+
+        # Regular expression for validating an email
+        email_regex = r"^[\w\.-]+@[\w\.-]+\.\w+$"
+
+        if not (re.fullmatch(phone_regex, value) or re.fullmatch(email_regex, value)):
+            raise serializers.ValidationError(rus['email_error'])
+
+        return value
+
